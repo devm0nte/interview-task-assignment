ae244d7199ad3d0cd02c6901d31decf342f5a288
devm0nte <pacharaphonk.dev@gmail.com>
2024-01-22 17:06:11 +0700

    add user status validate

--- src/middlewares/validators/user.validator.ts
+++ src/middlewares/validators/user.validator.ts
@@ -3,4 +3,5 @@ export const createUserValidator = [
     body("name").isString().notEmpty(),
     body("email").isEmail(),
     body("password").isString().notEmpty(),
+    body("role").optional().isIn(["User", "Admin"]).withMessage("role must be one of Admin/User"),
 ];
